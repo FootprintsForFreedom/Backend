dfa655303eaa4e468bb70f1211abcada01ad4544
niklhut <49069026+niklhut@users.noreply.github.com>
2023-07-17 18:05:28 +0200

    Improve configuration for release builds (#3)

--- Package.swift
+++ Package.swift
@@ -30,9 +30,18 @@ let package = Package(
         .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.50.4"),
     ],
     targets: [
-        .target(name: "AppApi", dependencies: [
-            .product(name: "SwiftDiff", package: "SwiftDiff"),
-        ]),
+        .target(
+            name: "AppApi",
+            dependencies: [
+                .product(name: "SwiftDiff", package: "SwiftDiff"),
+            ],
+            swiftSettings: [
+                // Enable better optimizations when building in Release configuration. Despite the use of
+                // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
+                // builds. See <https://github.com/swift-server/guides/blob/main/docs/building.md#building-for-production> for details.
+                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release)),
+            ]
+        ),
         .executableTarget(
             name: "App",
             dependencies: [
