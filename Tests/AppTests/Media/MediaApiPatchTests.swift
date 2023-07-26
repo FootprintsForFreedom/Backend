79f5ec5e69cde93e9c008b61ad214200f2400b95
niklhut <49069026+niklhut@users.noreply.github.com>
2023-07-26 19:30:24 +0200

    Improve naming

--- Tests/AppTests/Media/MediaApiPatchTests.swift
+++ Tests/AppTests/Media/MediaApiPatchTests.swift
@@ -227,9 +227,9 @@ final class MediaApiPatchTests: AppTestCase, MediaTest {
     func testPatchMediaWithIdFromOtherRepositoryFails() async throws {
         let token = try await getToken(for: .user, verified: true)
         let (repository, _, _) = try await createNewMedia(fileType: .document, verified: true)
-        let (_, _, _, updateContent) = try await getMediaPatchContent(patchedTitle: UUID().uuidString, verified: true)
+        let (_, _, _, patchContent) = try await getMediaPatchContent(patchedTitle: UUID().uuidString, verified: true)
 
-        let query = try URLEncodedFormEncoder().encode(updateContent)
+        let query = try URLEncodedFormEncoder().encode(patchContent)
 
         try app
             .describe("Patch media with media id from other repository should fail")
