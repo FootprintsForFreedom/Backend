43c6836378956cf915b5c76734225d983d720151
niklhut <49069026+niklhut@users.noreply.github.com>
2023-07-26 19:22:35 +0200

    Verify media file type on update and patch (#12)

--- Sources/App/Modules/Media/Controllers/MediaApiController.swift
+++ Sources/App/Modules/Media/Controllers/MediaApiController.swift
@@ -129,7 +129,7 @@ struct MediaApiController: ApiElasticDetailController, ApiElasticPagedListContro
 
         guard let mediaFileType = req.headers.contentType?.mediaFileType() else {
             if let fileType = req.headers.contentType {
-                req.logger.log(level: .critical, "A file with the following media type could not be uploaded: \(fileType.serialize()))")
+                req.logger.log(level: .notice, "A file with the following media type could not be uploaded: \(fileType.serialize()))")
                 throw Abort(.unsupportedMediaType, reason: "This content type is not supported.")
             } else {
                 throw Abort(.badRequest, reason: "No media file in body")
@@ -156,7 +156,7 @@ struct MediaApiController: ApiElasticDetailController, ApiElasticPagedListContro
         // file preparations
         guard let preferredFilenameExtension = req.headers.contentType?.preferredFilenameExtension() else {
             if let fileType = req.headers.contentType {
-                req.logger.log(level: .critical, "A file with the following media type could not be uploaded: \(fileType.serialize()))")
+                req.logger.log(level: .notice, "A file with the following media type could not be uploaded: \(fileType.serialize()))")
                 throw Abort(.unsupportedMediaType, reason: "This content type is not supported.")
             } else {
                 throw Abort(.badRequest, reason: "No media file in body")
@@ -228,6 +228,10 @@ struct MediaApiController: ApiElasticDetailController, ApiElasticPagedListContro
             guard let detailForFile = try await MediaDetailModel.find(mediaIdForFile, on: req.db) else {
                 throw Abort(.badRequest)
             }
+            try await detailForFile.$media.load(on: req.db)
+            guard detailForFile.media.fileType == repository.requiredFileType else {
+                throw Abort(.badRequest)
+            }
             detail.$media.id = detailForFile.$media.id
         } else {
             guard let mediaFileType = req.headers.contentType?.mediaFileType(), let preferredFilenameExtension = req.headers.contentType?.preferredFilenameExtension() else {
@@ -285,6 +289,9 @@ struct MediaApiController: ApiElasticDetailController, ApiElasticPagedListContro
 
         guard let mediaToPatch = try await MediaDetailModel.find(input.idForMediaDetailToPatch, on: req.db) else {
             throw Abort(.badRequest, reason: "No media with the given id could be found")
+        }
+        guard try mediaToPatch.$repository.id == repository.requireID() else {
+            throw Abort(.badRequest, reason: "Media to patch needs to be in same repository")
         }
 
         let mediaFileType = req.headers.contentType?.mediaFileType()
